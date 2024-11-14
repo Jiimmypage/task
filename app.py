64fbe00d1a2d7dbb69b14624102b0fe47bfad051
Jiimmypage <lluisfelipe1619@gmail.com>
2024-11-14 01:41:32 -0300

    initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+#CRUD
+#Create, Read, Update and Delete = Criar, Ler, Atualizar e Deletar
+#Tabela: Tarefa
+
+tasks = []
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
